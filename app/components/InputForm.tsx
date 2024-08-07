fb05623f20476fc5b181a415eed135a76407313a
= <=>
2024-08-07 17:04:11 +0700

    add validation

--- app/components/InputForm.tsx
+++ app/components/InputForm.tsx
@@ -10,24 +10,35 @@ export default function InputForm({ onSubmit }: any) {
     onSubmit({ teleID, amount });
   };
   return (
-    <form onSubmit={handleSubmit} className="flex justify-between flex-wrap mb-3">
-      <div className="mr-4 flex-1">
+    <form
+      onSubmit={handleSubmit}
+      className="flex justify-between flex-col mb-3"
+    >
+      <div className="mb-4 flex-1">
         <TextField
           label="Telegram ID"
           variant="outlined"
           onChange={(event) => setTeleID(event.target.value)}
+          error={/^(\d+(\.\d+)?|)$/.test(teleID) ? false : true}
+          helperText={/^(\d+(\.\d+)?|)$/.test(teleID) ? "" : "Please enter a valid number"}
+          required
         />
       </div>
-      <div className="mr-4 flex-1">
+      <div className="mb-4 flex-1">
         <TextField
           label="Amount"
           variant="outlined"
           onChange={(event) => setAmount(event.target.value)}
+          error={/^(\d+(\.\d+)?|)$/.test(amount) ? false : true}
+          helperText={/^(\d+(\.\d+)?|)$/.test(amount) ? "" : "Please enter a valid number"}
+          required
         />
       </div>
-      <Button type="submit" variant="contained" color="primary">
-        Run
-      </Button>
+      <div className="mx-auto">
+        <Button type="submit" variant="contained" color="primary">
+          Run
+        </Button>
+      </div>
     </form>
   );
 }
